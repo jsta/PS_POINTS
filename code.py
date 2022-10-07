65492ba8c7cf5ebacbae7dbbce1ea202259ca9e1
jsta <stachel2@msu.edu>
2022-10-07 12:56:11 -0600

    towards non-colab execution

--- code.py
+++ code.py
@@ -3,5 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-a=sns.load_dataset("rail_pspoints2")
-sns.replot(x="time", y="point", data =a, kind="line")
+# a=sns.load_dataset("rail_pspoints2")
+# sns.replot(x="time", y="point", data =a, kind="line")
